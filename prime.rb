8d7bfd2a2322b5f9f192e0c034bac86f790e177d
KJ Osenenko <karljosenenko@gmail.com>
2020-11-12 17:00:21 -0700

    commit tree efb31ca7ea7fc019c85bba1583d73b81982f8eef

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+require "pry"
+
+def prime?(int)
+    i = int
+    factors = []
+    if int.positive? && int != 1
+        while i > 0
+            if int % i == 0
+                factors  << i
+            end
+            i -= 1
+        end
+        if factors.length > 2
+            return false
+        else 
+            return true
+        end
+    else 
+        return false
+    end
+end
